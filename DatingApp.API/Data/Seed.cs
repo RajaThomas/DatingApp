10fa404e3a812e6264eb9f980996becb83d0a7c3
RajaThomas <68735077+RajaThomas@users.noreply.github.com>
2020-08-09 20:38:08 +0530

    end of section 8

---
+++ DatingApp.API/Data/Seed.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Linq;
+using DatingApp.API.Models;
+using Newtonsoft.Json;
+
+namespace DatingApp.API.Data
+{
+    public class Seed
+    {
+
+        public static void SeedUsers(DataContext _context)
+        {
+            if(!_context.Users.Any())
+            {
+                var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
+                var users = JsonConvert.DeserializeObject<List<User>>(userData);
+                foreach (var user in users)
+            {
+                //create the password hash
+                byte[] passwordHash, passwordSalt;
+                CreatePasswordHash("password", out passwordHash, out passwordSalt);
+
+                user.PasswordHash = passwordHash;
+                user.PasswordSalt = passwordSalt;
+                user.UserName = user.UserName.ToLower();
+
+                _context.Users.Add(user);
+            }
+
+            }
+            // seed users
+            
+            
+
+            _context.SaveChanges();
+        }
+
+        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
